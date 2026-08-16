7c694333288f65a1f14653ea6c0f3d7fa8af4c56
jackpilotsaas-ctrl <jackpilotsaas@gmail.com>
2026-08-16 23:01:42 +0200

    feat(analytics): integrate Google Analytics tag (gtag.js G-FM3KS33QN1) on all pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
+import Script from 'next/script';
 import './globals.css';
 
 const geistSans = Geist({
@@ -164,6 +165,20 @@ export default function RootLayout({ children }) {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FM3KS33QN1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-FM3KS33QN1');
+          `}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col selection:bg-orange-500 selection:text-white">
         <Toaster position="top-right" />
